b6b749d8a503b4edfdd599719a64e0f990fec2c0
rizkyauliaf <rizkyauliaf7@gmail.com>
2022-09-05 22:56:33 +0700

    Single Action Controller

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        //
+        return "Rizky Aulia Faridatuzzuhro dengan NIM 2241727037";
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\ArticleController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::get('/', [PageController::class, 'index']);
+
+// Route::get('/about', [PageController::class, 'about']);
+
+// Route::get('/articles/{id}', [PageController::class, 'articles']);
+
+// Route::get('/', [HomeController::class, '__invoke']);
+
+// Route::get('/', [AboutController::class, '__invoke']);
+
+Route::get('/{id}', [ArticleController::class, '__invoke']);
